1fa10a10f959352e73aa21028ad277f796d8207c
Xandor Schiefer <me@xandor.co.za>
2025-05-19 16:45:07 +0200

    fix: string paths may not contain `..`
    > `..` path components in string paths are not supported, neither as inputs nor as outputs.
    >
    > — `nixpkgs/lib/path/README.md`

--- modules/programs/browserpass.nix
+++ modules/programs/browserpass.nix
@@ -25,40 +25,40 @@ in {
       if x == "brave" then
         let
           dir = if isDarwin then
-            "Library/Application Support/BraveSoftware/Brave-Browser/NativeMessagingHosts"
+            "Library/Application Support/BraveSoftware/Brave-Browser"
           else
-            ".config/BraveSoftware/Brave-Browser/NativeMessagingHosts";
+            ".config/BraveSoftware/Brave-Browser";
         in [{
           # Policies are read from `/etc/brave/policies` only
           # https://github.com/brave/brave-browser/issues/19052
-          "${dir}/com.github.browserpass.native.json".source =
+          "${dir}/NativeMessagingHosts/com.github.browserpass.native.json".source =
             "${pkgs.browserpass}/lib/browserpass/hosts/chromium/com.github.browserpass.native.json";
         }]
       else if x == "chrome" then
         let
           dir = if isDarwin then
-            "Library/Application Support/Google/Chrome/NativeMessagingHosts"
+            "Library/Application Support/Google/Chrome"
           else
-            ".config/google-chrome/NativeMessagingHosts";
+            ".config/google-chrome";
         in [{
-          "${dir}/com.github.browserpass.native.json".source =
+          "${dir}/NativeMessagingHosts/com.github.browserpass.native.json".source =
             "${pkgs.browserpass}/lib/browserpass/hosts/chromium/com.github.browserpass.native.json";
-          "${dir}/../policies/managed/com.github.browserpass.native.json".source =
+          "${dir}/policies/managed/com.github.browserpass.native.json".source =
             "${pkgs.browserpass}/lib/browserpass/policies/chromium/com.github.browserpass.native.json";
         }]
       else if x == "chromium" then
         let
           dir = if isDarwin then
-            "Library/Application Support/Chromium/NativeMessagingHosts"
+            "Library/Application Support/Chromium"
           else
-            ".config/chromium/NativeMessagingHosts";
+            ".config/chromium";
         in [
           {
-            "${dir}/com.github.browserpass.native.json".source =
+            "${dir}/NativeMessagingHosts/com.github.browserpass.native.json".source =
               "${pkgs.browserpass}/lib/browserpass/hosts/chromium/com.github.browserpass.native.json";
           }
           {
-            "${dir}/../policies/managed/com.github.browserpass.native.json".source =
+            "${dir}/policies/managed/com.github.browserpass.native.json".source =
               "${pkgs.browserpass}/lib/browserpass/policies/chromium/com.github.browserpass.native.json";
           }
         ]
@@ -86,13 +86,13 @@ in {
       else if x == "vivaldi" then
         let
           dir = if isDarwin then
-            "Library/Application Support/Vivaldi/NativeMessagingHosts"
+            "Library/Application Support/Vivaldi"
           else
-            ".config/vivaldi/NativeMessagingHosts";
+            ".config/vivaldi";
         in [{
-          "${dir}/com.github.browserpass.native.json".source =
+          "${dir}/NativeMessagingHosts/com.github.browserpass.native.json".source =
             "${pkgs.browserpass}/lib/browserpass/hosts/chromium/com.github.browserpass.native.json";
-          "${dir}/../policies/managed/com.github.browserpass.native.json".source =
+          "${dir}/policies/managed/com.github.browserpass.native.json".source =
             "${pkgs.browserpass}/lib/browserpass/policies/chromium/com.github.browserpass.native.json";
         }]
       else
